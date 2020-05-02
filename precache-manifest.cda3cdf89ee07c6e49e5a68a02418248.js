c0609d63bcb9564dd85da52eeb4909f5d4d0ff0d
stardollarstar <stardollarstar@gmail.com>
2020-05-02 19:44:23 +0800

    Updates

---
+++ precache-manifest.cda3cdf89ee07c6e49e5a68a02418248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca852dd2f7dc188f81c9d49613e9096",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "12e1a4d3549a3bee2423",
+    "url": "/reactapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a0807612eceab01ab3c1",
+    "url": "/reactapp/static/js/2.b12c7bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.b12c7bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e1a4d3549a3bee2423",
+    "url": "/reactapp/static/js/main.18e911ce.chunk.js"
+  },
+  {
+    "revision": "ac6066f36cef9b249f60",
+    "url": "/reactapp/static/js/runtime-main.17e8f566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
